74ad538b813230ff1389073d6d9af229f1c7eff9
diegonamk <you@example.com>
2020-06-16 01:03:03 +0200

    Start

---
+++ COVID.js
@@ -0,0 +1,12 @@
+flag = false;
+function test() {
+    if (flag===true){
+        document.getElementById("test").innerHTML = "4BIT-COVID_ICTFORMA";
+        flag=false;
+    }
+    else{
+        document.getElementById("test").innerHTML = "by Diego Namkhai";
+        flag=true;
+    }
+  
+}
